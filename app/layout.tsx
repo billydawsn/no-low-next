1339ed3273a3dab1c48e1401102ae536445bfb48
billydawsn <34036440+billydawsn@users.noreply.github.com>
2024-03-30 09:02:10 +0000

    Added Analytics Packages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Head from "next/head";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 import "bootstrap-icons/font/bootstrap-icons.css";
